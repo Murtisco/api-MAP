c5fa976241ae0b756a43054f1883856824aa0511
Murtisco <francisco.juliao@aluno.unifametro.edu.br>
2019-11-29 22:36:14 -0300

    resre
    rewsre
    
    Co-Authored-By: murtisco <murtisco@users.noreply.github.com>

---
+++ WebApi/Controllers/ClientesController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    public class ClientesController : ApiController
+    {
+        private static List<Cliente> clientes = new List<Cliente>();
+
+        public List<Cliente> Get()
+        {
+            return clientes;
+        }
+
+        public void Post(string nome, string data, string cpf, string email)
+        {
+            if (!string.IsNullOrEmpty(nome))
+            {
+                clientes.Add(new Cliente(nome, data, cpf, email));
+            }
+        }
+    }
+}
---
+++ WebApi/Models/Cliente.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApi.Models
+{
+    public class Cliente
+    {
+        public String Nome { get; set; }
+        public String DataNascimento { get; set; }
+        public String CPF { get; set; }
+        public String Email { get; set; }
+
+        public Cliente(string nome, string data, string cpf, string email)
+        {
+            this.Nome = nome;
+            this.DataNascimento = data;
+            this.CPF = cpf;
+            this.Email = email;
+
+        }
+    }
+}
